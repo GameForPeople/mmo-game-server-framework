8da741c344d6b616c30c7d0f61267096b70e09ad
GameForPeople <Koreagamemaker@gmail.com>
2019-03-29 02:58:49 +0900

    Define, ServerDefine 분리

--- Server/Define.h
+++ Server/Define.h
@@ -5,12 +5,18 @@
 		- 해당 헤더 파일은, 서버와 클라이언트가 공통으로 사용합니다. 
 		(클라이언트에서 서버 프로젝트 코드 참조)
 
-	#0. 해당 헤더는, Enum, Static Constexp
+	#0. 해당 헤더는, Enum class, enum, Constexpr 상수 만을 포함해야합니다.
+	
+	!0. enum, 전역 상수의 경우, 반드시 namespace안에 선언되어야 합니다.
+	!1. enum class, enum의 경우 마지막에 ENUM_SIZE를 포함해야합니다.
 */
 
 namespace GLOBAL_DEFINE
 {
 	constexpr USHORT SERVER_PORT = 9000;
+
+	// 추후 해당 헤더에서 추방, SERVER_DEFINE으로 위치 변경 필요합니다.
+	constexpr BYTE MAX_CLIENT = 10;
 }
 
 namespace PACKET_TYPE
---
+++ Server/ServerDefine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/*
+	ServerDefine.h
+		- 해당 헤더 파일은, 서버에서만 사용합니다.
+*/
+
+namespace SERVER_DEFINE
+{
+
+}

32b00364553952cbf7ec023d506eda501266dcb2
GameForPeople <Koreagamemaker@gmail.com>
2019-03-30 02:01:44 +0900

    Recv, Send, Logout 프로세스 ServerDefine에 정의
    Recv, Send, Logout 프로세스 ServerDefine에 정의

--- Server/ServerDefine.cpp
+++ Server/ServerDefine.cpp
@@ -1,103 +1,165 @@
 #include "pch.h"
-
+#include "SocketInfo.h"
+#include "SendMemoryPool.h"
+#include "Scene.h"
 #include "ServerDefine.h"
 
-namespace GLOBAL_UTIL
+namespace NETWORK_UTIL
 {
-	namespace BIT_CONVERTER
+	/*
+		SendPacket()
+			- WSASend!는 여기에서만 존재할 수 있습니다.
+
+		!0. 단순히 WSA Send만 있는 함수입니다. 데이터는 준비해주세요.
+		!1. wsaBuf의 len, wsaBuf의 buf는 지정해주셔야합니다.
+	*/
+	void SendPacket(SocketInfo* pClient, SendMemoryUnit* pSendMemoryUnit)
 	{
-		_NODISCARD BYTE MakeSendPacket(const BYTE inPacketType) noexcept { return inPacketType | SEND_BYTE; }
+		// 받은 데이터에 대한 처리가 끝나면 바로 다시 받을 준비.
+		DWORD flag{};
 
-		_NODISCARD bool GetRecvOrSend(const char inChar) noexcept { return (inChar >> 7) & (0x01); }
+		ZeroMemory(&pSendMemoryUnit->overlapped, sizeof(pSendMemoryUnit->overlapped));
 
-		/*
-			MakeByteFromLeftAndRightByte
-				- 0x10보다 작은 바이트 두개를 인자로 받아(Left, Right) 상위 4개비트는 Left, 하위 4개 비트는 Right를 담아반환합니다.
-
-			!0. 인자로 들어오는 두 바이트 모두, 0x10보다 작은 값이 들어와야합니다.
-				- 인자로 들어오는 Left 바이트가 0x0f보다 큰 값이 들어올 경우, 오버플로우되어 비정상적인 값이 반횐될 수 있음.
-				- 인자로 들어오는 Right 바이트가 0x0f보다 큰 값이 들어올 경우, LeftByte의 | 연산에서 비정상적인 값을 반환할 수 있음.
-		*/
-		_NODISCARD BYTE MakeByteFromLeftAndRightByte(const BYTE inLeftByte, const BYTE inRightByte) noexcept
-		{
-			return (inLeftByte << 4) | (inRightByte);
-		}
-
-		/*
-			GetLeft4Bit
-				- HIWORD(?)와 유사하게 동작합니다. 하나의 바이트를 받아서 상위(좌측) 4개의 비트를 바이트로 변환해서 반환합니다.
-		*/
-		_NODISCARD BYTE GetLeft4Bit(const BYTE inByte) noexcept
-		{
-			return (inByte >> 4) & (0x0f);
-		}
-
-		/*
-			GetRight4Bit
-				- LOWORD(?)와 유사하게 동작합니다. 하나의 바이트를 받아서 하위(우측) 4개의 비트를 바이트로 변환해서 반환합니다.
-		*/
-		_NODISCARD BYTE GetRight4Bit(const BYTE inByte) noexcept
-		{
-			return (inByte) & (0x0f);
-		}
+		ERROR_HANDLING::errorRecvOrSendArr[
+			static_cast<bool>(
+				1 + WSASend(pClient->sock, &pSendMemoryUnit->wsaBuf, 1, NULL, 0, &pSendMemoryUnit->overlapped, NULL)
+				)
+		]();
 	}
 
-	namespace ERROR_HANDLING
+	/*
+		RecvPacket()
+			- WSA Recv는 여기서만 존재합니다.
+
+		!0. SocketInfo에 있는 wsaBuf -> buf 에 리시브를 합니다. 
+		!1. len은 고정된 값을 사용합니다. MAX_SIZE_OF_RECV_BUFFER!
+	*/
+	void RecvPacket(SocketInfo* pClient)
 	{
-		/*
-			ERROR_QUIT
-				- 서버에 심각한 오류가 발생할 경우, 메세지 박스를 활용해 에러를 출력하고, 서버를 종류합니다.
-		*/
-		_NORETURN void ERROR_QUIT(const WCHAR *msg)
+		// 받은 데이터에 대한 처리가 끝나면 바로 다시 받을 준비.
+		DWORD flag{};
+
+		ZeroMemory(&pClient->overlapped, sizeof(pClient->overlapped));
+
+		ERROR_HANDLING::errorRecvOrSendArr[
+			static_cast<bool>(
+				1 + WSARecv(pClient->sock, &pClient->wsaBuf, 1, NULL, &flag /* NULL*/, &pClient->overlapped, NULL)
+				)
+		]();
+	}
+
+	/*
+		LogOutProcess()
+			- 나갑니다~~~
+	*/
+	void LogOutProcess(SocketInfo* pClient)
+	{
+		SOCKADDR_IN clientAddr;
+
+		int addrLength = sizeof(clientAddr);
+
+		getpeername(pClient->sock, (SOCKADDR *)&clientAddr, &addrLength);
+		std::cout << " [GOODBYE] 클라이언트 (" << inet_ntoa(clientAddr.sin_addr) << ") 가 종료했습니다. \n";
+		if (pClient->clientContIndex != -1) pClient->pScene->ExitClient(pClient->clientContIndex);
+
+		closesocket(pClient->sock);
+		delete pClient;
+	}
+}
+
+namespace BIT_CONVERTER
+{
+	_NODISCARD BYTE MakeSendPacket(const BYTE inPacketType) noexcept { return inPacketType | SEND_BYTE; }
+
+	_NODISCARD bool GetRecvOrSend(const char inChar) noexcept { return (inChar >> 7) & (0x01); }
+
+	/*
+		MakeByteFromLeftAndRightByte
+			- 0x10보다 작은 바이트 두개를 인자로 받아(Left, Right) 상위 4개비트는 Left, 하위 4개 비트는 Right를 담아반환합니다.
+
+		!0. 인자로 들어오는 두 바이트 모두, 0x10보다 작은 값이 들어와야합니다.
+			- 인자로 들어오는 Left 바이트가 0x0f보다 큰 값이 들어올 경우, 오버플로우되어 비정상적인 값이 반횐될 수 있음.
+			- 인자로 들어오는 Right 바이트가 0x0f보다 큰 값이 들어올 경우, LeftByte의 | 연산에서 비정상적인 값을 반환할 수 있음.
+	*/
+	_NODISCARD BYTE MakeByteFromLeftAndRightByte(const BYTE inLeftByte, const BYTE inRightByte) noexcept
+	{
+		return (inLeftByte << 4) | (inRightByte);
+	}
+
+	/*
+		GetLeft4Bit
+			- HIWORD(?)와 유사하게 동작합니다. 하나의 바이트를 받아서 상위(좌측) 4개의 비트를 바이트로 변환해서 반환합니다.
+	*/
+	_NODISCARD BYTE GetLeft4Bit(const BYTE inByte) noexcept
+	{
+		return (inByte >> 4) & (0x0f);
+	}
+
+	/*
+		GetRight4Bit
+			- LOWORD(?)와 유사하게 동작합니다. 하나의 바이트를 받아서 하위(우측) 4개의 비트를 바이트로 변환해서 반환합니다.
+	*/
+	_NODISCARD BYTE GetRight4Bit(const BYTE inByte) noexcept
+	{
+		return (inByte) & (0x0f);
+	}
+}
+
+namespace ERROR_HANDLING
+{
+	/*
+		ERROR_QUIT
+			- 서버에 심각한 오류가 발생할 경우, 메세지 박스를 활용해 에러를 출력하고, 서버를 종류합니다.
+	*/
+	_NORETURN void ERROR_QUIT(const WCHAR *msg)
+	{
+		LPVOID lpMsgBuf;
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL,
+			WSAGetLastError(),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&lpMsgBuf,
+			0,
+			NULL
+		);
+
+		MessageBox(NULL, (LPTSTR)lpMsgBuf, msg, MB_ICONERROR);
+		LocalFree(lpMsgBuf);
+		exit(1);
+	};
+
+
+	/*
+		ERROR_DISPLAY
+			- 서버에 오류가 발생할 경우, 에러 로그를 출력합니다.
+	*/
+	/*_DEPRECATED*/ void ERROR_DISPLAY(const CHAR *msg)
+	{
+		LPVOID lpMsgBuf;
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL,
+			WSAGetLastError(),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&lpMsgBuf,
+			0,
+			NULL
+		);
+
+		printf(" [%s]  %s", msg, (LPTSTR)&lpMsgBuf);
+		LocalFree(lpMsgBuf);
+	};
+
+	/*
+		HandleRecvOrSendError
+			- Send Or Recv 실패 시 출력되는 함수로, 에러를 출력합니다.
+	*/
+	void HandleRecvOrSendError()
+	{
+		if (WSAGetLastError() != ERROR_IO_PENDING)
 		{
-			LPVOID lpMsgBuf;
-			FormatMessage(
-				FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-				NULL,
-				WSAGetLastError(),
-				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				(LPTSTR)&lpMsgBuf,
-				0,
-				NULL
-			);
-
-			MessageBox(NULL, (LPTSTR)lpMsgBuf, msg, MB_ICONERROR);
-			LocalFree(lpMsgBuf);
-			exit(1);
-		};
-
-
-		/*
-			ERROR_DISPLAY
-				- 서버에 오류가 발생할 경우, 에러 로그를 출력합니다.
-		*/
-		/*_DEPRECATED*/ void ERROR_DISPLAY(const CHAR *msg)
-		{
-			LPVOID lpMsgBuf;
-			FormatMessage(
-				FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-				NULL,
-				WSAGetLastError(),
-				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				(LPTSTR)&lpMsgBuf,
-				0,
-				NULL
-			);
-
-			printf(" [%s]  %s", msg, (LPTSTR)&lpMsgBuf);
-			LocalFree(lpMsgBuf);
-		};
-
-		/*
-			HandleRecvOrSendError
-				- Send Or Recv 실패 시 출력되는 함수로, 에러를 출력합니다.
-		*/
-		void HandleRecvOrSendError()
-		{
-			if (WSAGetLastError() != ERROR_IO_PENDING)
-			{
-				ERROR_DISPLAY(("RecvOrSend()"));
-			}
+			ERROR_DISPLAY(("RecvOrSend()"));
 		}
 	}
 }

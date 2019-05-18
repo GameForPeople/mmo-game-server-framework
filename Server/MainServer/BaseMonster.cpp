4811b0be0cfb4e79bb6bc68b540680822bfdaec4
GameForPeople <Koreagamemaker@gmail.com>
2019-05-19 03:41:13 +0900

    [Main] 몬스터 변수 설정 및 모델 개념 도입

--- Server/MainServer/BaseMonster.cpp
+++ Server/MainServer/BaseMonster.cpp
@@ -3,12 +3,30 @@
 #include "ObjectInfo.h"
 #include "BaseMonster.h"
 
-BaseMonster::BaseMonster(_KeyType inKey, _PosType inX, _PosType inY)
+BaseMonster::BaseMonster(_KeyType inKey, _PosType inX, _PosType inY, const MonsterModel* const inMonsterModel)
+	: objectInfo(nullptr)
+	, monsterModel(inMonsterModel)
+	, level()
+	, spawnPosX(inX)
+	, spawnPosY(inY)
+	, isDead(false)
+	, noDamageTick(0)
+	, faintTick(0)
+	, freezeTick(0)
+	, electricTick(0)
+	, burnTick(0)
 {
 	objectInfo = new ObjectInfo(inKey, inX, inY);
+	level = ( rand() % monsterModel->levelMaxDifference ) + monsterModel->startLevel;
+
+	hp = monsterModel->hpPerLevel * level;
+	damage = monsterModel->damagePerLevel * level;
 }
 
 BaseMonster::~BaseMonster()
 {
+	// 몬스터 모델은 삭제되서는 안됩니다.
+	// monsterModel = nullptr;
+
 	delete objectInfo;
 }
